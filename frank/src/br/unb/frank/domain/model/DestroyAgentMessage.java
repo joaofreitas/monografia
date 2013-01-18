e45bf2715174ba14909965c650be17ef6666a14a
João Paulo <joao.freitas@seatecnologia.com.br>
2013-01-18 01:14:33 -0200

    Tentativa de matar agentes após o logout do usuário

---
+++ frank/src/br/unb/frank/domain/model/DestroyAgentMessage.java
@@ -0,0 +1,15 @@
+package br.unb.frank.domain.model;
+
+public class DestroyAgentMessage {
+
+    private Long alunoId;
+
+    public Long getAlunoId() {
+	return alunoId;
+    }
+
+    public void setAlunoId(Long alunoId) {
+	this.alunoId = alunoId;
+    }
+
+}
